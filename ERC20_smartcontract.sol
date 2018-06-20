8b35277c8dd4739feea98d6a16017ff6f15646f3
Keyur Patel <simkeyur@gmail.com>
2018-06-20 18:27:48 -0500

    Update ERC20_smartcontract.sol

--- ERC20_smartcontract.sol
+++ ERC20_smartcontract.sol
@@ -108,7 +108,7 @@ contract ERC20Coin is StandardToken { // CHANGE THIS. Update the contract name.
         balances[msg.sender] = 1000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 1000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "ERC20Coin";                                   // Set the name for display purposes (CHANGE THIS)
-        decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
+        decimals = 5;                                               // Amount of decimals for display purposes (CHANGE THIS)
         symbol = "SYMBOL";                                          // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
